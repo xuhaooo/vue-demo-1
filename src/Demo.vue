0555a7b7148853b4f4aabb10b6c679f595007caf
xuhao <bingoxuhao@gmail.com>
2021-03-10 22:23:17 +0800

    Vue起手式 CRM学习了Vue的一些构造选项

---
+++ src/Demo.vue
@@ -0,0 +1,18 @@
+<template>
+     <div class="red">
+         <button @click="fn">call fn</button>
+         {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['message', 'fn']
+}
+</script>
+
+<style scoped>
+.red {
+    color: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+// import Vue from 'vue'
+// import App from './App.vue'
+const Vue = window.Vue
 Vue.config.productionTip = false
+import Demo from './Demo.vue'
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  components: {Demo},
+  data: {
+    visible: true,
+    n: 0
+  },
+  template: `
+    <div>
+      {{n}}
+      <button @click="toggle">toggle</button>
+      <hr>
+      <Demo :message="n" :fn="add"/>
+    </div>
+  `,
+  methods: {
+    add(){
+      this.n += 1
+    },
+    toggle(){
+      this.visible = !this.visible
+    }
+  }
+}).$mount('#frank')
